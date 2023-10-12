7fa1223f201b39f8b22c5215b523be75f8f2726b
jgarzon97 <jorgegarzonizquierdo@gmail.com>
2023-10-11 20:22:58 -0500

    registros para las tablas

--- src/app/database/basededatos.sql
+++ src/app/database/basededatos.sql
@@ -30,3 +30,28 @@ CREATE TABLE Vehiculo (
     id_usuario INT,
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
+
+-- Roles para el sistema
+INSERT INTO Rol (tipo_rol, detalles_rol) VALUES
+('administrador', 'Rol con permisos de administrador.'),
+('invitado', 'Rol para los invitados.');
+
+-- Usuario con rol "administrador"
+INSERT INTO Usuario (email, pass, nombre, apellido, estado, id_rol) VALUES
+('jorgegarzon@gmail.com', 'admin', 'Jorge', 'Garzón', 'Activo', 1);
+
+-- Usuarios con rol "invitado"
+INSERT INTO Usuario (mail, pass, nombre, apellido, estado, id_rol) VALUES
+('invitado@gmail.com', '11111', 'invitado1', 'invitado1', 'Activo', 2);
+
+--
+INSERT INTO Vehiculo (codigo, placa, tipo_registro, descripcion, precio, anticipo, id_usuario)
+VALUES
+    (100, 'XYZ123', 'preventivo', 'Sedán rojo', 15000.00, 0.00, 1),
+    (101, 'ABC456', 'mantenimiento', 'SUV negro', 200.00, 0.00, 1),
+    (102, 'DEF789', 'correctivo', 'Hatchback azul', 12000.00, 0.00, 1),
+    (103, 'GHI012', 'flete', 'Camioneta gris', 250.00, 50.00, 1),
+    (104, 'JKL567', 'preventivo', 'Sedán plateado', 18000.00, 0.00, 2),
+    (105, 'MNO890', 'correctivo', 'Hatchback blanco', 11000.00, 0.00, 2),
+    (106, 'PQR123', 'flete', 'Camioneta roja', 280.00, 60.00, 2),
+    (107, 'STU456', 'preventivo', 'Sedán negro', 16000.00, 0.00, 3);
